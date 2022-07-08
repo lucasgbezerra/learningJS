9aa91ac2bc90dbb0a3b08944237e67f6869915a8
Lucas <lucasbsb100@gmail.com>
2022-07-08 14:23:02 -0300

    Fix a bug

--- memoryGame/src/scripts.js
+++ memoryGame/src/scripts.js
@@ -2,28 +2,37 @@ const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let disableClick = false
 
 function flipCard() {
     this.classList.toggle('flip');
 
+    if (disableClick)
+        return;
+
+    console.log(disableClick)
     if (!hasFlippedCard) {
         firstCard = this;
         hasFlippedCard = true;
-        console.log("firstCard " + { hasFlippedCard, firstCard });
-    } else {
+    }else{
         secondCard = this;
         hasFlippedCard = false;
-        console.log("Second:" + { hasFlippedCard, secondCard })
         checkMatch();
     }
+
+
 }
 
 // Virar a carta para posição original
 function turnCardBack() {
+    disableClick = true;
+
     setTimeout(
         () => {
             firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip')
+            secondCard.classList.remove('flip');
+
+            disableClick = false;
         }, 1000
     );
 }
@@ -37,17 +46,15 @@ function disableCards() {
 
 // Confere se as cartas são iguais
 function checkMatch() {
+    console.log(firstCard.getAttribute('data-name'), secondCard.getAttribute('data-name'))
     if (firstCard.getAttribute('data-name') === secondCard.getAttribute('data-name')) {
         disableCards();
         return true;
     }
-    turnCardBack();
-    return false;
-
+        turnCardBack();
+        return false
 }
-
-
 
 // Listener para todas as cartas, ativado com o clique e chama a
 // função flipCard
-cards.forEach(card => card.addEventListener('click', flipCard))
+cards.forEach(card => card.addEventListener('click', flipCard));
