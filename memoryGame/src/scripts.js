f30e516b274841fae29812d56d9139f0c09754f5
Lucas <lucasbsb100@gmail.com>
2022-07-08 08:17:02 -0300

    Add match check to card pair

--- memoryGame/src/scripts.js
+++ memoryGame/src/scripts.js
@@ -1,9 +1,43 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
 
-function flipCard(){
+function flipCard() {
     this.classList.toggle('flip')
+
+    if (!hasFlippedCard) {
+        firstCard = this;
+        hasFlippedCard = true;
+        console.log("firstCard " + { hasFlippedCard, firstCard });
+    } else {
+        secondCard = this;
+        hasFlippedCard = false;
+        console.log("Second:" + { hasFlippedCard, secondCard })
+        checkMatch();
+    }
 }
+
+
+// Desabilita o listener de click do par de cartas
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard)
+    secondCard.removeEventListener('click', flipCard)
+}
+
+
+// Confere se as cartas são iguais
+function checkMatch() {
+    if (firstCard.getAttribute('data-name') === secondCard.getAttribute('data-name')) {
+        disableCards()
+        return true
+    }
+    return false
+
+}
+
+
+
 // Listener para todas as cartas, ativado com o clique e chama a
 // função flipCard
 cards.forEach(card => card.addEventListener('click', flipCard))
